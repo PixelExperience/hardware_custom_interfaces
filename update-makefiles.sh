71685577d081e042a14c943d8c0a745941e617ef
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-09 08:50:28 -0300

    Remove lineage power hal

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -3,6 +3,5 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.lineage:hardware/custom-interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"
